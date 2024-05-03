fdb670b523ce9decbcc789f5134ccc76337dd65a
saadriia <[saadriasr005@gmail.com]>
2024-05-03 10:02:10 +0300

    addBottun

--- lib/view/widgets/Add_note_ButtonSheet.dart
+++ lib/view/widgets/Add_note_ButtonSheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/view/widgets/Constants.dart';
 import 'package:notes_app/view/widgets/Custom_TexetField.dart';
 
 
@@ -21,8 +22,34 @@ class AddNoteButtonSheat extends StatelessWidget {
           SizedBox(height: 16,),
             CustomTexetField(
               hint: 'content',
-              maxLines: 5, )
+              maxLines: 5, ),
+                        SizedBox(height: 16,),
+
+              customButton()
         ],
+      ),
+    );
+  }
+}
+class customButton extends StatelessWidget {
+  const customButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: kPrimaryColor,),
+      
+       child: Center(
+        child: Text('Submet', style:
+         TextStyle(
+          color: Colors.black87,
+          fontSize: 24, 
+
+        ), ),
       ),
     );
   }
